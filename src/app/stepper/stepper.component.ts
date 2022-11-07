9dc94a69b209d3c4d2520a4ee64503cadb8f23b5
danrusu <danginkgo@yahoo.com>
2022-11-07 15:42:28 +0200

    update

---
+++ src/app/stepper/stepper.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+
+@Component({
+  selector: 'app-stepper',
+  template: `
+    <div>
+      <button aria-label="decrement" (click)="decrement()">-</button>
+      <span data-cy="counter">{{ count }}</span>
+      <button aria-label="increment" (click)="increment()">+</button>
+    </div>
+  `,
+})
+export class StepperComponent {
+  @Input() count = 0
+  @Output() change = new EventEmitter()
+
+  increment(): void {
+    this.count++
+    this.change.emit(this.count)
+  }
+
+  decrement(): void {
+    this.count--
+    this.change.emit(this.count)
+  }
+}
